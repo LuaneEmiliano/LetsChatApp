46cca2ad1273c20896b3c6a1f5f11658eab355f4
luane <luaneemiliano@gmail.com>
2022-07-13 13:37:04 -0700

    Initial Commit

---
+++ LetsChatApp/LetsChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsChatAppApp.swift
+//  LetsChatApp
+//
+//  Created by luane Niejelski on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct LetsChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
